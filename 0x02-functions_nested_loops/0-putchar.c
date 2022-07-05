19b9f9b471eeb58896f7692811788523d150e122
ananiabahru <ananiabaharu13@gmail.com>
2022-07-05 12:00:41 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
-#include <ubistd.h>
 #include "main.h"
-
 /**
- * main - description 
- * description: Write a program that prints _putchar, followed by a new line
- * Return: Always 0
- */
-main(void)
+* main - description 
+* description: Write a program that prints _putchar, followed by a new line
+* Return: Always 0
+*/
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-        _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 
-	return (0);
 }
