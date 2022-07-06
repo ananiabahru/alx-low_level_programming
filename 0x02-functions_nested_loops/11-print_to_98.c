2634255cd34815c96f598cdfdd7c6cb36fa6efcf
ananiabahru <ananiabaharu13@gmail.com>
2022-07-05 17:20:19 -0700

    fix 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
 * print_to_98 - Check description
 * @n: A input integer
-* Description: It prints whole number n - 98
+* Description: It prints whole number starting from n to 98
 * and 98 to n in order
 * Return: Nothing.
 */
