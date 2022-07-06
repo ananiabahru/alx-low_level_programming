619bb2c79c48fe9eddbd58e713364cf4011a2a13
ananiabahru <ananiabaharu13@gmail.com>
2022-07-05 18:37:24 -0700

    hot fix task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,16 @@
+#include <unistd.h>
 #include "main.h"
-
 /**
-* main -prints putchar followd by newline 
+* main - prints_putchar using the function called main 
+* Description: It prints the word Holberton, followed by a new line.
 * Return: Always 0
 */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char word[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+_putchar(word[i]);
 _putchar('\n');
 return (0);
 }
