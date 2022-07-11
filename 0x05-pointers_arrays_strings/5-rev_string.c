a542423d05a6beecb26e158f8f7bfa21e6093f14
ananiabahru <ananiabaharu13@gmail.com>
2022-07-11 11:43:51 -0700

    task 5 hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,9 @@ length2 = 0;
 while (s[length1] != ('\0'))
 length1++;
 
-length2 =length1 - 1
+length2 =length1 - 1;
 for (i = 0; i < length1 / 2; i++)
 {
-"sammyschool" "loohcsymmas"
 tmp = s[i];
 s[i] = s[length2];
 s[length2] = tmp;
