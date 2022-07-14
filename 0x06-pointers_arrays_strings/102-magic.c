ce9ac0a5dfa1e115d1faa2eb6411281cf886ff0b
anania <106769157+ananiabahru@users.noreply.github.com>
2022-07-14 23:45:07 +0300

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main(void)
 {
   int n;
@@ -6,7 +7,7 @@ int main(void)
   int *p;
 
   a[2] = 1024;
-  p = &n;
+  p[5] = 98;
   /*
    * write your line of code here...
    * Remember:
