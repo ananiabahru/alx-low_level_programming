9f77e78846c7f1a4c3ad514cf04ba6248b90bb81
ananiabahru <anaiabaharu13@gmail.com>
2022-07-18 18:31:42 +0300

    task 0 hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <string.h>
 /**
  * _memset -> function that fills memory with a constant byte.
  * @s: is string 
@@ -8,8 +8,5 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a;
-for (a = 0; a < n; a++)
-s[a] = b;
-return (s);
+return (memset(s, b, n));
 }
