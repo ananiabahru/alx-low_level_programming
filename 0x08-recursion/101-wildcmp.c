d5d684cb4138e67ca155802264c9dc19053ffe6d
ananiabahru <ananiabaharu13@gmail.com>
2022-07-20 13:42:14 -0700

    fix task 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,9 @@
 * @s2: one string
 * @after_wldcd: placeholder for position right after wildcard
 * Return: 1 if matched, 0 if not
+*
 */
+
 int substring_match(char *s1, char *s2, char *after_wldcd)
 {
 if (*s1 == '\0' && *s2 == '\0')
@@ -17,8 +19,9 @@ return (0);
 if (*s2 == '*')
 return (substring_match(s1, s2 + 1, s2 + 1));
 if (*s1 == *s2)
-else
 return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+else
+return (substring_match(s1 + 1, after_wldcd, after_wldcd));
 }
 /**
 * wildcmp - compare if string with wildcard mattches
