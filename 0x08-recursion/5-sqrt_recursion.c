adf309f205e2c7da31c8eb0a6caa4c001134735c
ananiabahru <ananiabaharu13@gmail.com>
2022-07-20 10:34:51 -0700

    fix task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,20 +6,21 @@
 */
 int _sqrt_recursion(int n)
 {
-return (_sqrt(n, 1));
+return (_sqrt_recursion(n, 1));
+
 }
 /**
-* _sqrt_recursion -> recursive square root
+* _sqrt -> recursive square root
 * @n: number
 * @i: iterator
 * Return: a number
 */
-int _sqrt_recursion(int n, int i)
+int _sqrt(int n, int i)
 {
 int square = i * i;
 if (square > n)
 return (-1);
 if (square == n)
 return (i);
-}
 return (_sqrt(n, i + 1));
+}
