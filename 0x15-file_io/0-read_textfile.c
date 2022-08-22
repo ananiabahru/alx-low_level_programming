44db440c9e87733ee9ab389c371addc737adbf86
anania <106769157+ananiabahru@users.noreply.github.com>
2022-08-23 01:05:46 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,36 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 /**
-* read_textfile-> reads a text file and prints it to the POSIX standard output.
-* @filename: name of the file.
-* @letters: number of letters.
-* Return: actual number of letters it could read and print
-*/
+  * read_textfile - reads a text file and prints to the POSIX stdout.
+  * @filename: pointer to name of file.
+  * @letters:  number of letters it should read and print.
+  *
+  * Return: number of letters it could print or write or 0.
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t lenr, lenw;
-char *buffer;
-if (filename == NULL)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
-close(fd);
-return (0);
-}
-lenr = read(fd, buffer, letters);
-close(fd);
-if (lenr == -1)
-{
-free(buffer);
-return (0);
-}
-lenw = write(STDOUT_FILENO, buffer, lenr);
-free(buffer);
-if (lenr != lenw)
-return (0);
-return (lenw);
+	int file, rd, wr;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+	rd = read(file, buf, letters);
+	if (rd == -1)
+		return (0);
+
+	buf[letters] = '\0';
+
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
+
+	close(file);
+	free(buf);
+	return (wr);
 }
