dc15026bbfd2f17d97003c58d2839e94908a7d6e
ananiabahru <ananiabaharu13@gmail.com>
2022-08-22 14:54:35 -0700

    fix task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,9 +4,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 /**
-* read_textfile ->  reads a text file and prints it to the POSIX standard output.
-* @filename: name of the file 
+* read_textfile-> reads a text file and prints it to the POSIX standard output.
+* @filename: name of the file.
 * @letters: number of letters.
+* Return: actual number of letters it could read and print
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
